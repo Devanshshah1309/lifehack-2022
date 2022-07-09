8b5d077b59376f599111024b0e60d3a8c873242d
HarveySpecter <devanshshah1309@gmail.com>
2022-07-09 21:48:33 +0800

    Add basic marketplace UI

---
+++ pages/marketplace.js
@@ -0,0 +1,22 @@
+import Sidebar from "../components/Sidebar/Sidebar";
+import { Box, Flex, HStack, VStack, Heading, Text } from "@chakra-ui/react";
+import Items from "../components/Items/items";
+import { useAuth } from "../context/AuthContext";
+export default function Marketplace() {
+  const { user } = useAuth();
+  return (
+    <HStack>
+      <Sidebar />
+      <VStack paddingLeft={10} width="100%">
+        <Heading>
+          Welcome to the Marketplace,
+          {user.name === undefined ? "John Doe" : user.name}
+        </Heading>
+        <Text fontSize="2xl">
+          Below, you can see all the items available for you to trade.
+        </Text>
+        <Items />
+      </VStack>
+    </HStack>
+  );
+}
