7ea900ddebfc3b0d0cf187ecca6ad9ca3eb18998
HarveySpecter <devanshshah1309@gmail.com>
2022-07-09 18:13:30 +0800

    Standardise component folder

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import {
 } from "@chakra-ui/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { DesktopNav } from "../components/Navbar";
+import { DesktopNav } from "../components/Navbar/index";
 
 const img = require("../assets/fruits.jpeg");
 
